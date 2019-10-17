bd96212cf9313c3b957119e6f3720a8cba42398a
INEKE <inekesusilowati89@gmail.com>
2019-10-18 02:30:37 +0700

    Sebuah Fragment dapat berkomunikasi dengan menerima argumen atau parameter. Pola yang umum digunakan yaitu dengan menggunakan method static newInstance untuk membuat Fragment dengan argumen. Hal ini dikarenakan sebuah Fragment sebaiknya hanya mempunya contructor tanpa adanya argumen.

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -3,11 +3,15 @@ package id.ac.polinema.idealbodyweight;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import id.ac.polinema.idealbodyweight.Fragments.AboutFragment;
+
 public class MainActivity extends AppCompatActivity {
+	private AboutFragment aboutFragment;
 
 	// Deklarasikan atribut Fragment di sini
 
@@ -15,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		aboutFragment = AboutFragment.newInstance("Ineke Susilowati");
 	}
 
 	@Override
@@ -27,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
 	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 		// TODO: Tambahkan penanganan menu di sini
 
+		if (item.getItemId() == R.id.menu_about) {
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.fragment_container, aboutFragment)
+					.addToBackStack(null)
+					.commit();
+		}
 		return super.onOptionsItemSelected(item);
 	}
-}
